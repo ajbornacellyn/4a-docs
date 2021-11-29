90de1c2ef31f47c74b0df176a94398f689a3305c
StormShadowm <ma.morecruz@gmail.com>
2021-11-29 10:12:41 -0500

    shipents_ms

--- shipment_ms/src/main/java/com/team5/shipment_ms/repositories/DeliveryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.team5.shipment_ms.repositories;
-
-import com.team5.shipment_ms.models.Delivery;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface DeliveryRepository extends MongoRepository<Delivery, String> {
-
-}
