6d5f5d517b1aefb115e036a42ef87ae719750b2b
William Quistial <williquisti@gmail.com>
2021-11-29 08:41:26 -0500

    creacion de models y repositories para el proyecto del microservicio MakeShipment_MS

---
+++ shipment_ms/shipment_ms/src/test/java/com/team5/shipment_ms/ShipmentMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team5.shipment_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipmentMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
